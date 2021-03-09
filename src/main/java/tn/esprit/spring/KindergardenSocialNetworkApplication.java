18f3c7f8552734842e5033c637cd9f89c7e9a2bc
arij <arijbenmansour1@gmail.com>
2021-03-09 16:31:59 +0100

    last11

--- src/main/java/tn/esprit/spring/KindergardenSocialNetworkApplication.java
+++ src/main/java/tn/esprit/spring/KindergardenSocialNetworkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KindergardenSocialNetworkApplication {
 
 	public static void main(String[] args) {
+		int a;
 		SpringApplication.run(KindergardenSocialNetworkApplication.class, args);
 	}
 
